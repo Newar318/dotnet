b39ed40fb5e3ca5fab8218b319a50c8025284d1d
Asbin <asbinsherstha309@gmail.com>
2024-07-22 10:29:56 +0545

    All set

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿﻿using System;
+using System.Collections;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Welcome to QFI movies!");
+
+        Console.WriteLine("Please Enter your name for movies list:");
+
+        string name = Console.ReadLine();
+        User u = new User();
+        u.setUsername(name);
+        if (u.getUsername().Equals("asbin", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"Welcome to QFI movies! Mr/Mrs {u.getUsername()}");
+
+            Movie m1 = new Movie();
+            m1.setTitle("Avengers");
+
+            Console.WriteLine("Now showing:");
+
+            Movie m = new Movie();
+            m.setTitle("Avengers: Endgame");
+            List<Movie> li = new List<Movie>();
+            li.Add(m);
+            li.Add(m1);
+            li.Add(new Movie("Salaar"));
+            li.Add(new Movie("Pathan"));
+            li.Add(new Movie("KGF 2"));
+            li.Add(new Movie("RRR"));
+            li.Add(new Movie("Bhool Bhulaiyaa 2"));
+            foreach (var movie in li)
+            {
+                Console.WriteLine(movie.getTitle());
+            }
+
+        }
+         
+        else
+        {
+            Console.WriteLine("Unauthorized access!!!");
+        }
+
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,47 @@
+public class Ticket
+{
+    private String name;
+    private String seat;
+    private String time;
+    private String date;
+    private String price;
+    private String movieName;
+    private String movieTime;
+    private String movieDate;
+    private String moviePrice;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+
+    public String getSeat()
+    {
+        return seat;
+    }
+
+    public void setSeat(String seat)
+    {
+        this.seat = seat;
+    }
+
+    public void setTime(String time)
+    {
+        this.time = time;
+    }
+
+    public String getTime()
+    {
+        return time;
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
